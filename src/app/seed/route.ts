7209e9add580f69d33a646f6d8a51035240f632e
= <=>
2024-09-11 17:02:56 -0300

    update

--- src/app/seed/route.ts
+++ src/app/seed/route.ts
@@ -1,7 +1,7 @@
 import { db } from '@vercel/postgres'
 import { users } from '../lib/databaseSeed'
 
-const client = await db.connect()
+const client = await db.connect();
 
 async function seedUsers() {
   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`
@@ -11,7 +11,7 @@ async function seedUsers() {
        user TEXT NOT NULL UNIQUE,
        password TEXT NOT NULL
      );
-   `
+   `;
 
   const insertedUsers = await Promise.all(
     users.map(async (user) => {
@@ -20,21 +20,21 @@ async function seedUsers() {
          INSERT INTO users (id, user, password)
          VALUES (${user.id}, ${user.user}, ${user.password})
          ON CONFLICT (id) DO NOTHING;
-       `
-    })
-  )
+       `;
+    }),
+  );
 
   return insertedUsers
 }
 
 export async function GET() {
   try {
-    await client.sql`BEGIN`
-    await seedUsers()
-    await client.sql`COMMIT`
-    return Response.json({ message: 'Database seeded successfully' })
+    await client.sql`BEGIN`;
+    await seedUsers();
+    await client.sql`COMMIT`;
+    return Response.json({ message: 'Database seeded successfully' });
   } catch (error) {
-    await client.sql`ROLLBACK`
-    return Response.json({ error }, { status: 500 })
+    await client.sql`ROLLBACK`;
+    return Response.json({ error }, { status: 500 });
   }
 }
