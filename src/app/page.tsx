6e5cf1a1d8dab135494e1af112592b78e2c93ee2
= <=>
2024-09-11 13:12:52 -0300

    update actions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
 'use client'
 
-import { usePathname } from "next/navigation"
-
-
 const page = () => {
-  const pathname = usePathname()
   return (
     <>
-    <p>La ruta actual es: {pathname}</p>
-    <div>page</div>
+      <div>page</div>
     </>
   )
 }
